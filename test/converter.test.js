167eb286e0f9147c23a8fb40d8cbbcbb4f81085d
rhapsodyn <rhapsodyn@gmail.com>
2017-03-08 23:27:30 +0800

    add some test

---
+++ test/converter.test.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+const converter = require('../converter.js');
+
+suite("Converter Tests(coz it cant be debug)", () => {
+
+    test("toMd", () => {});
+
+    test('toEnml', () => {});
+});
