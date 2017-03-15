0763812b8a6543a37ec0288c27b8dbaea35fc36c
rhapsodyn <rhapsodyn@gmail.com>
2017-03-15 17:57:02 +0800

    add some warnings; add some statusbar msg

--- evernote-adapter.js
+++ evernote-adapter.js
@@ -6,23 +6,35 @@ const config = vscode.workspace.getConfiguration("evernote");
 
 const MAX_QUERY_COUNT = 99; //TODO only get 99 notes of a notebook
 
-var client, noteStore;
+let client, noteStore;
+
+function showStatusBarMsg(msg) {
+    vscode.window.setStatusBarMessage(msg, 1000);
+}
 
 function listNoteBooks() {
+    showStatusBarMsg("Requesting notebooks......");
+    
     client = new Evernote.Client({token: config.token});
     noteStore = client.getNoteStore(config.noteStoreUrl);
     return noteStore.listNotebooks();
 }
 
 function listAllNoteMetas(notebookGuid) {
+    showStatusBarMsg("Requesting all note metas......");
+    
     return noteStore.findNotesMetadata({notebookGuid}, 0, MAX_QUERY_COUNT, {includeTitle: true});
 }
 
 function getNoteContent(noteGuid) {
+    showStatusBarMsg("Requesting note content......");
+    
     return noteStore.getNoteContent(noteGuid);
 }
 
 function updateNoteContent(guid, title, content) {
+    showStatusBarMsg("Updating note content......");
+    
     return noteStore.updateNote({guid, title, content});
 }
 
--- test/converter.test.js
+++ test/converter.test.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 const converter = require('../converter.js');
 
-suite("Converter Tests(coz it cant be debug)", () => {
+suite('Converter Tests(coz it cant be debug)', () => {
 
     test('toMd', () => {
         let enml = '<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd"><en-note style=";">'
